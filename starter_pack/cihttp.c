4298bb29c7f9060bc776d22b8a26803477200b5d
kevinGreen630 <kevin.green630@myci.csuci.edu>
2019-02-24 15:50:41 -0800

    Fixed

--- starter_pack/cihttp.c
+++ starter_pack/cihttp.c
@@ -1,5 +1,7 @@
 #include "cihttp.h"
 
+typedef int bool;
+enum { false, true };
 void *GetInAddr(struct sockaddr *sa)
 {
     if (sa->sa_family == AF_INET)
@@ -15,6 +17,7 @@ void AddHeaderToRequest(struct httprequest *req, char *name, char *value)
     NVL *header = malloc(sizeof(NVL));
     header->name = name;
     header->value = value;
+    //printf("\nADDING %s %s", header->name, header->value);
     if (req->headers) 
         header->next = req->headers;
     else
@@ -40,14 +43,140 @@ struct httprequest *ParseHTTPRrequest(char *data)
     req = malloc(sizeof(struct httprequest));
 
     // TODO: incomplete
-    //printf("%s", data);
-    char* token = strtok(data, " ");
+     //READS THROUGH ENTIRE STRING
+    
+    bool methodAssigned = false;
+    int i = 0;
+    //printf("%s\n", methodRequestVersion);
+    char *token;
+    token = strtok(data, " \r\n");
 
-    //READS THROUGH ENTIRE STRING
+    //HANDLE METHOD
+    
+    for(i = 0; !methodAssigned && i < 3; i++)
+    {
+        if(strcmp(token, implemented_methods[i]) == 0)
+        {
+            methodAssigned = true;
+            req->method = i;
+        }
+    }
+    if(!methodAssigned)
+    {
+        //THROW EXCEPTION HERE WHEN IMPLEMENTED
+        printf("ERROR: Method not recognized\n");
+        return NULL;
+    }
+    //HANDLE REQUEST
+    token = strtok(NULL, " \n");
+    //printf("%s", methodRequestVersion);
+    req->request_uri = token;
+
+    //HANDLE VERSION
+    char Version[20];
+    token = strtok(NULL," \n");
+    strcpy(Version, token);
+    //char *temp = Version[5];
+    for(i = 0; Version[i+5] != '\0'; i++)
+        Version[i] = Version[i+5];
+    Version[i] = '\0';
+    req->version = atof(Version);
+    token = strtok(NULL, "\r\n");
+
+    //HANDLING THE REST
+    /*
+    char temp1[256];
+    char temp2[256];
+    //if(strstr(token, "Host:"))
+    //    printf("TESTING!\n");
+    strcpy(temp1, token);
+    //printf("%s == %s --- ", token, temp1);
+    token = strtok(NULL, "\r\n");
+    strcpy(temp2, token);
+    //printf("%s == %s --- ", token, temp2);
+    //printf("%s && %s\n", temp1,temp2);
+    //printf("%s && %s\n", temp1,temp2);
+    AddHeaderToRequest(req, temp1, temp2);
+    //token = strtok(NULL, "\r\n");
+    token = strtok(NULL, " \n");
+    */
+    while(token != NULL)
+    {
+        char *temp1a;
+        char *temp2a;
+        //printf("NAME: %s -- ", token);
+        temp1a = (char*) malloc(strlen(token) * sizeof(char));
+        strcpy(temp1a, token);
+        token = strtok(NULL, "\r\n");
+        if(token != NULL)
+            {
+                temp2a = (char*) malloc(strlen(token) * sizeof(char));
+                //printf(" VALUE %s\n", token);
+                strcpy(temp2a, token);
+                AddHeaderToRequest(req, temp1a, temp2a);
+                token = strtok(NULL, " \n");
+            }
+    }
+
+    //DO NOT DELETE
     /*while(token != NULL)
     {
-        printf("%s", token);
-        token = strtok(NULL, " ");
+        printf("%s\n", token);
+        token = strtok(NULL, "\r\n");
+    }*/
+
+    //printf("TESTING\n%s\n", token);
+
+    /*
+    //THIS STUFF WORKS FOR NOW
+    token = strtok(data, " \n");
+    //HANDLE METHOD
+
+    //char temp[strlen(token)];
+    //for(i = 0; i < strlen(token); i++)
+        //temp[i] = token[i];
+
+    //char *methodRequestVersion = strtok(temp, " ");
+    
+    for(i = 0; !methodAssigned && i < 3; i++)
+    {
+        if(strcmp(token, implemented_methods[i]) == 0)
+        {
+            methodAssigned = true;
+            req->method = i;
+        }
+    }
+    if(!methodAssigned)
+    {
+        //THROW EXCEPTION HERE WHEN IMPLEMENTED
+        printf("ERROR: Method not recognized\n");
+        return NULL;
+    }
+    //HANDLE REQUEST
+    token = strtok(NULL, " \n");
+    //printf("%s", methodRequestVersion);
+    req->request_uri = token;
+
+
+    //HANDLE VERSION
+    char Version[20];
+    token = strtok(NULL," \n");
+    strcpy(Version, token);
+    //char *temp = Version[5];
+    for(i = 0; Version[i+5] != '\0'; i++)
+        Version[i] = Version[i+5];
+    Version[i] = '\0';
+    req->version = atof(Version);
+    token = strtok(NULL," \n");
+   
+    printf("REMAINING INFOMRATION\n");
+    while(token != NULL)
+    {   
+        //printf("%c\n", token[strlen(token)-1]);
+        if(token[strlen(token) -1] == ':')
+            //printf("Start Header:\n");
+        printf("%s\n", token);
+        token = strtok(NULL, " \n");
     }*/
     return req;
 }
@@ -55,12 +184,28 @@ struct httprequest *ParseHTTPRrequest(char *data)
 void PrintHTTPRequest(struct httprequest *req)
 {
     // TODO: incomplete
+    //printf("\n--HTTP REQUEST DATA--\n");
+    printf("HTTP %.2f %s Request: \n", req->version,  implemented_methods[req->method]);
+    printf("Request-URI: %s\n", req->request_uri);
+    NVL *headers = req->headers;
+    while(headers != NULL)
+    {
+        printf("        %s %s\n", headers->name, headers->value);
+        headers = headers->next;
+    }
+    /*
+    //THE FOLLOWING IS FOR TESTING INDIVIDUAL HTTP REQUESTS
+    char *temp;
+    scanf("%s", temp);
+    */
+    //printf("\n")
+    
 }
 
 struct httpresponse *GenerateHTTPResponse(struct httprequest *req)
 {
     struct httpresponse *res = malloc(sizeof(struct httpresponse));
-
+    ptintf("Hello\n");
     // TODO: incomplete
 
     return res;
